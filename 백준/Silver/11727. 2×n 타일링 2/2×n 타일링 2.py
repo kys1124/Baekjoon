5ed52520e0175ef76405ee950f4b3d40f0262d60
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-08-31 09:20:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n==1:
+    print(1)
+elif n==2:
+    print(3)
+else:
+    dp = [0]*(n+1)
+
+    dp[1]=1
+    dp[2]=3
+
+    for i in range(3,n+1):
+        dp[i] = (dp[i-1]+2*dp[i-2])%10007
+
+    print(dp[n])
