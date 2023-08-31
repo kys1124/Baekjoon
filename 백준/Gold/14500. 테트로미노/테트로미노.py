032368147949d7d49045b75aad1d6b48ec00cd20
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-09-01 08:52:58 +0900

    [Gold IV] Title: 테트로미노, Time: 216 ms, Memory: 119616 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,41 +1,34 @@
-N, M = map(int, input().split())
+N,M = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
+
 ans = 0
 
-def dfs(n, sm, lst,cnt):
-    global ans,v, mxv
-    if ans>= sm + (4-cnt)* mxv:
-        return
-    
-    if n==2:
-        mx = 0
-        for si,sj in lst:
-            for di,dj in ((1,0),(0,1),(0,-1)):
-                ni,nj = si+di, sj+dj
-                if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]>mx:
-                    mx = arr[ni][nj]
-        ans = max(ans, sm+mx)
+def dfs(n, sm, lst):
+    global ans
+    if ans>=sm+(3-n)*1000:
         return
 
-    for si,sj in lst:
-        for di,dj in ((1,0),(0,1),(0,-1)):
-            ni,nj = si+di, sj+dj
+    if n==3:
+        ans = max(ans, sm)
+        return
+
+
+    for ci,cj in lst:
+        for di, dj in ((-1, 0), (0, 1), (0, -1)):
+            ni,nj = di+ci, dj+cj
             if 0<=ni<N and 0<=nj<M and v[ni][nj]==0:
                 v[ni][nj]=1
-                dfs(n+1, sm+arr[ni][nj], lst+[(ni,nj)], cnt+1)
+                dfs(n+1, sm+arr[ni][nj], lst+[(ni,nj)])
                 v[ni][nj]=0
 
 v = [[0]*M for _ in range(N)]
-mxv = 0
-for i in range(N):
-    for j in range(M):
-        mxv = max(mxv, arr[i][j])
+
 
 for i in range(N):
     for j in range(M):
         v[i][j]=1
-        dfs(0,arr[i][j], [(i,j)],1)
+        dfs(0,arr[i][j], [(i,j)])
         v[i][j]=0
 
 print(ans)
