e315205c4ecc0a05a427bd3c17b9495df4bd6ad6
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-09-12 20:50:36 +0900

    [Gold I] Title: 낚시왕, Time: 288 ms, Memory: 133380 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,59 +1,60 @@
 import sys
-input= sys.stdin.readline
-
+input = sys.stdin.readline
 R,C,M = map(int, input().split())
 
-shark =dict()
-dir = {0:(-1,0), 1:(1,0), 2:(0,1), 3:(0,-1)}
-change = {0:1,1:0, 2:3,3:2}
+shark = [[0]*C for _ in range(R)]
+dir = {1:(-1,0), 2:(1,0), 3:(0,1), 4:(0,-1)}
+change = {1:2,2:1, 3:4,4:3}
 for _ in range(M):
     r,c,s,d,z = map(int, input().split())
-    shark[(r-1,c-1)] = (s,d-1,z)
+    if d==1 or d==2:
+        shark[r-1][c-1] = (s%(2*(R-1)),d,z)
+    else:
+        shark[r-1][c-1] = (s%(2*(C-1)),d,z)
+
 
 sm = 0 # 잡은 상어 크기 누적.
 for idx in range(C): # 낙시꾼 위치
     for j in range(R): #해당 낙시꾼 위치에서 상어 찾기
-        if shark.get((j,idx)):
-            sm+=shark[(j,idx)][2]
-            del shark[(j,idx)]
+
+        if shark[j][idx]!=0:
+            sm += shark[j][idx][2]
+            shark[j][idx]= 0
             break
 
     # 상어 이동 구현
-    new_shark = dict()
-    for key, value in shark.items():
-        ci,cj = key
-        cs,cd,cz = value
+    new_shark = [[0]*C for _ in range(R)]
+    for i in range(R):
+        for j in range(C):
+            if shark[i][j]!=0:
+                ci,cj = i,j
+                cs,cd,cz = shark[ci][cj]
+                ni = ci + cs * dir[cd][0]
+                nj = cj + cs * dir[cd][1]
+                ns = cs
+                while not (0 <= ni < R and 0 <= nj < C):
+                    if ni < 0:
+                        ns -= ci
+                        ci = 0
+                    elif ni >= R:
+                        ns -= (R - 1) - ci
+                        ci = R - 1
+                    elif nj < 0:
+                        ns -= cj
+                        cj = 0
+                    elif nj >= C:
+                        ns -= (C - 1) - cj
+                        cj = C - 1
+                    cd = change[cd]
+                    ni, nj = ci + ns * dir[cd][0], cj + ns * dir[cd][1]
 
-        if cd==0 or cd==1: #2*(R-1) 혹은 2*(C-1) 칸 이동하면 자기 자리로 돌아옴.
-             cs%=(2*(R-1))
-        else:
-            cs%=(2*(C-1))
+                ci, cj = ni, nj
 
-        ni = ci + cs * dir[cd][0]
-        nj = cj + cs * dir[cd][1]
-        ns = cs
-        while not (0 <= ni < R and 0 <= nj < C):
-            if ni < 0:
-                ns -= ci
-                ci = 0
-            elif ni >= R:
-                ns -= (R - 1) - ci
-                ci = R - 1
-            elif nj < 0:
-                ns -= cj
-                cj = 0
-            elif nj >= C:
-                ns -= (C - 1) - cj
-                cj = C - 1
-            cd = change[cd]
-            ni, nj = ci + ns * dir[cd][0], cj + ns * dir[cd][1]
-
-        ci,cj = ni,nj
-        if  not new_shark.get((ci,cj)):
-            new_shark[(ci,cj)] = (cs,cd,cz)
-        else:
-            if new_shark[(ci,cj)][2]<cz:
-                new_shark[(ci,cj)] = (cs,cd,cz)
+                if new_shark[ci][cj]==0:
+                    new_shark[ci][cj] = (cs,cd,cz)
+                else:
+                    if new_shark[ci][cj][2]<cz:
+                        new_shark[ci][cj]=(cs,cd,cz)
 
     shark = new_shark
 print(sm)
