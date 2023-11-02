44b9f4dc81795315cb4150edd73f0f00069438b1
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-02 19:44:53 +0900

    [Gold II] Title: 통나무 옮기기, Time: 164 ms, Memory: 114184 KB -BaekjoonHub

--- 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.py
+++ 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.py
@@ -1,77 +1,83 @@
 from collections import deque
-N = int(input())
-arr = [list(input()) for _ in range(N)]
-def find_strat():
-    q = []
+N = int(input()) #  NxN 격자
+arr = [list(input()) for _ in range(N)] # 1은 이동 불가, 0이동가능
+
+q = deque()
+
+def find(s):
     for i in range(N):
         for j in range(N):
-            if arr[i][j]=='B':
-                if j+1<N and arr[i][j+1]=='B':
-                    q.append((i,j+1,0))
-                    arr[i][j]=arr[i][j+1]=arr[i][j+2]='0'
-                    return q
-                else:
-                    q.append((i+1,j,1))
-                    arr[i][j]=arr[i+1][j]=arr[i+2][j]='0'
-                    return q
-def find_end():
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j]=='E':
-                if j+1<N and arr[i][j+1]=='E':
-                    ei,ej,e_flag = i,j+1,0
-                    arr[i][j]=arr[i][j+1]=arr[i][j+2]='0'
-                    return ei,ej,e_flag
-                else:
-                    ei,ej, e_flag = i+1,j,1
-                    arr[i][j]=arr[i+1][j]=arr[i+2][j]='0'
-                    return ei,ej,e_flag
-ei,ej,e_flag = find_end()
-# q에 중심 좌표만 넣고 생각하기.
-def check(ci,cj):
-    for r in (-1,0,1):
-        for c in (-1,0,1):
-            if arr[ci+r][cj+c]=='1':
+            if arr[i][j]==s:
+                if i+1<N and arr[i+1][j]==s:
+                    arr[i][j]='0'
+                    arr[i+1][j]='0'
+                    arr[i+2][j]='0'
+                    return i+1,j,0
+                elif j+1<N and arr[i][j+1]==s:
+                    arr[i][j]='0'
+                    arr[i][j+1]='0'
+                    arr[i][j+2]='0'
+                    return i,j+1,1
+
+si,sj,s_flag = find('B')
+q.append((si,sj,s_flag)) #통나무의 중심 좌표와 세로or 가로 상태 기록.
+ei,ej,e_flag = find('E')
+
+def check(ci,cj,flag):
+    if flag==0:
+        if arr[ci][cj]=='0' and arr[ci-1][cj]=='0' and arr[ci+1][cj]=='0':
+            return True
+        else:
+            return False
+
+    elif flag==1:
+        if arr[ci][cj]=='0' and arr[ci][cj-1]=='0' and arr[ci][cj+1]=='0':
+            return True
+        else:
+            return False
+
+def check2(ci,cj):
+    for i in (-1,0,1):
+        for j in (-1,0,1):
+            ni,nj = ci+i,cj+j
+            if arr[ni][nj]=='1':
                 return False
     return True
 
-def bfs():
-    q = find_strat()
-    q = deque(q)
-    v = [[[0] * N for _ in range(N)] for _ in range(2)]  # 0은 가로 1은 세로 통나무
-    si,sj,s_flag = q[0]
+
+def bfs(q):
+    v = [[[0] * N for _ in range(N)] for _ in range(2)]
     v[s_flag][si][sj]=1
-    cnt = 1
+    cnt = 0 #횟수 기록.
     while q:
         for _ in range(len(q)):
-            ci,cj, flag = q.popleft()
+            ci,cj,flag = q.popleft()
             if (ci,cj,flag)==(ei,ej,e_flag):
-                return v[e_flag][ei][ej]-1
+                return v[e_flag][ei][ej]
 
-            if flag==0: #가로 누워있는 경우
-                for di,dj in (1,0),(0,1),(0,-1),(-1,0):
-                    ni,nj = ci+di,cj+dj
-                    if 0<=ni<N and 1<=nj<N-1 and (v[flag][ni][nj]==0 or v[flag][ni][nj]>cnt+1):
-                        if arr[ni][nj]=='0' and arr[ni][nj+1]=='0' and arr[ni][nj-1]=='0':
-                            v[flag][ni][nj]=cnt+1
-                            q.append((ni,nj,flag))
-
-                if 1<=ci<N-1 and 1<=cj<N-1 and (v[1][ci][cj]==0 or v[1][ci][cj]>cnt+1) and check(ci,cj):
+            if flag==0: # 세로 통나무
+                for di,dj in ((1,0),(-1,0),(0,1),(0,-1)): #4방향 이동
+                    ni,nj = ci+di, cj+dj
+                    if 1<=ni<N-1 and 0<=nj<N and v[flag][ni][nj]==0 and check(ni,nj,flag):
+                        v[flag][ni][nj]=cnt+1
+                        q.append((ni,nj,flag))
+                # T 회전
+                if 0<=ci<N and 1<=cj<N-1 and v[1][ci][cj]==0 and check2(ci,cj):
                     v[1][ci][cj]=cnt+1
                     q.append((ci,cj,1))
 
-            else: # 세로
-                for di,dj in (1,0),(0,1),(0,-1),(-1,0):
-                    ni,nj = ci+di,cj+dj
-                    if 1<=ni<N-1 and 0<=nj<N and (v[flag][ni][nj]==0 or v[flag][ni][nj]>cnt+1):
-                        if arr[ni-1][nj]=='0' and arr[ni][nj]=='0' and arr[ni+1][nj]=='0':
-                            v[flag][ni][nj]=cnt+1
-                            q.append((ni,nj,flag))
+            else: # 가로 통나무
+                for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
+                    ni, nj = ci+di, cj+dj
+                    if 0<=ni<N and 1<=nj<N-1 and v[flag][ni][nj]==0 and check(ni,nj,flag):
+                        v[flag][ni][nj]=cnt+1
+                        q.append((ni,nj,flag))
 
-                if 1<=ci<N-1 and 1<=cj<N-1 and (v[0][ci][cj]==0 or v[0][ci][cj]>cnt+1) and check(ci,cj):
+                # T 회전
+                if 1<=ci<N-1 and 0<=cj<N and v[0][ci][cj]==0 and check2(ci,cj):
                     v[0][ci][cj]=cnt+1
                     q.append((ci,cj,0))
         cnt+=1
-    return 0
 
-print(bfs())
+    return 0
+print(bfs(q))
