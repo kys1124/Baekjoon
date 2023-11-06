443eb1ea1b39bd96d7b0e1accf4713f1670fed82
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-06 20:42:14 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 268 ms, Memory: 114700 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,81 +1,76 @@
-N,M = map(int, input().split())
-arr = [['#']*(M+2)] + [['#']+list(map(str, input()))+['#'] for _ in range(N)] + [['#']*(M+2)]
+N, M = map(int, input().split())
+arr = [list(map(str, input())) for _ in range(N)]
+dir = {0:(1,0),1:(-1,0),2:(0,1),3:(0,-1)}
 
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        if arr[i][j] == 'O':
-            ei, ej = i, j
-        elif arr[i][j] == 'B':
-            bi, bj = i, j
+for i in range(N):
+    for j in range(M):
+        if arr[i][j]=='R':
+            ri,rj = i,j
+        elif arr[i][j]=='B':
+            bi,bj = i,j
 
-        elif arr[i][j] == 'R':
-            ri, rj = i, j
+        elif arr[i][j]=='O':
+            ei,ej = i,j
 
-dir = {0:(1,0),1:(-1,0), 2:(0,1),3:(0,-1)}
+
 
 ans = 11
-def dfs(n, bi,bj, ri, rj,d):
-    global ans,v
-    if n>10:
+
+def dfs(ri,rj,bi,bj,d, cnt):
+    global ans,arr
+    if cnt>10:
+        return
+    
+    if ans<=cnt: # 가지치기
+        return
+    if (bi,bj)==(ei,ej):
         return
 
-    if bi==ei and bj==ej:
+    if (ri,rj)==(ei,ej) and (bi,bj)!=(ei,ej): # 파란구슬도 같이 빠지면 정답 갱신 x
+        ans = min(ans, cnt) # 빨간 구슬만 빠지면 정답 갱신
         return
 
-    if (ri,rj)==(ei,ej) and (bi,bj)!=(ei,ej):
-        ans = min(n, ans)
-        return
-
-    for  i in range(4):
+    for i in range(4):
         if (d==0 or d==1) and (i==0 or i==1):
             continue
         elif (d==2 or d==3) and (i==2 or i==3):
             continue
-
-        q = [(bi,bj,1),(ri,rj,0)] # 0이 빨간 공 1이 파란공
+        q = [(ri, rj, 0), (bi, bj, 1)]  # 0은 빨간색, 1은 파란색
         if i==0:
-           q.sort(key=lambda x:-x[0])
-        elif i==1:
             q.sort(key=lambda x:x[0])
+        elif i==1:
+            q.sort(key=lambda x:-x[0])
         elif i==2:
-            q.sort(key=lambda x:-x[1])
-        else:
             q.sort(key=lambda x:x[1])
-
-        while q:
-            for _ in range(len(q)):
-                ci,cj, color = q.pop(0)
-                ni,nj = ci+dir[i][0], cj+dir[i][1]
-                arr[ci][cj]='.'
+        else:
+            q.sort(key=lambda x:-x[1])
+        copy_arr = [arr[i][:] for i in range(N)]
+        for _ in range(len(q)):
+            ci,cj, color = q.pop()
+            arr[ci][cj]='.'
+            for mul in range(1,max(M,N)):
+                ni,nj = ci+mul*dir[i][0], cj+mul*dir[i][1]
                 if arr[ni][nj]=='.':
-                    q.append((ni,nj, color))
-
+                    continue
                 elif arr[ni][nj]=='O':
                     if color==0:
                         nri,nrj = ni,nj
-                        re1 = 'O'
                     else:
                         nbi,nbj = ni,nj
-                        re2 = 'O'
-
+                    break
                 else:
                     if color==0:
-                        nri,nrj = ci,cj
-                        re1 = arr[nri][nrj]
+                        nri,nrj = ci+(mul-1)*dir[i][0], cj+(mul-1)*dir[i][1]
                         arr[nri][nrj]='R'
                     else:
-                        nbi,nbj = ci,cj
-                        re2 = arr[nbi][nbj]
+                        nbi,nbj = ci+(mul-1)*dir[i][0], cj+(mul-1)*dir[i][1]
                         arr[nbi][nbj]='B'
+                    break
+        dfs(nri,nrj,nbi,nbj, i, cnt+1)
+        arr = copy_arr
 
-        dfs(n+1, nbi,nbj,nri,nrj,i)
-        arr[ri][rj]='R'
-        arr[bi][bj]='B'
-        arr[nri][nrj]=re1
-        arr[nbi][nbj]=re2
-
-dfs(0,bi,bj,ri,rj,-1)
-if ans<11:
-    print(ans)
-else:
+dfs(ri,rj,bi,bj,-1,0)
+if ans==11:
     print(-1)
+else:
+    print(ans)
