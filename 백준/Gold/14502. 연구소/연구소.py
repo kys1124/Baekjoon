f888bf583fbe0eaf971045d36e20f5b62e0c38fd
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-07 20:32:35 +0900

    [Gold IV] Title: 연구소, Time: 364 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,11 +1,7 @@
-import sys
-input = sys.stdin.readline
-
 from collections import deque
 
-N,M = map(int, input().split())
+N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
 empty = []
 virus = []
 for i in range(N):
@@ -14,43 +10,37 @@ for i in range(N):
             empty.append((i,j))
         elif arr[i][j]==2:
             virus.append((i,j))
+total_cnt = len(empty)
 
-l = len(empty)
-
-
-def dfs(n,s, lst):
-    global ans
-    if n==3:
-        for i in lst:
-            arr[empty[i][0]][empty[i][1]]=1
-        cnt = bfs()
-        ans = max(ans, cnt)
-        for i in lst:
-            arr[empty[i][0]][empty[i][1]]=0
-        return
-
-
-    for i in range(s,l):
-        dfs(n+1, i+1, lst+[i])
-
-
-def bfs():
+def bfs(arr,total_cnt):
     q = deque(virus)
     v = [[0]*M for _ in range(N)]
-    cnt = l-3
     for ci,cj in virus:
         v[ci][cj]=1
-
     while q:
         ci,cj = q.popleft()
         for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
-            ni,nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]==0:
+            ni,nj = ci+di,cj+dj
+            if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]!=1:
+                if arr[ni][nj]==0:
+                    total_cnt-=1
                 v[ni][nj]=1
                 q.append((ni,nj))
-                cnt -=1
-    return cnt
+    return total_cnt
 
 ans = 0
+def dfs(n, s, lst):
+    global ans
+    if n==3:
+        for ci,cj in lst:
+            arr[ci][cj]=1
+        ans = max(ans, bfs(arr,total_cnt-3))
+        for ci,cj in lst:
+            arr[ci][cj]=0
+        return
+
+    for i in range(s,len(empty)):
+        dfs(n+1, i+1, lst+[empty[i]])
+
 dfs(0,0,[])
 print(ans)
