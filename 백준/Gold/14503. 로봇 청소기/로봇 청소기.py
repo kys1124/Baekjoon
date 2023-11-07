beb2aed8b3660660b8ff8055f6376090e1aa025a
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-07 20:14:52 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,35 +1,36 @@
-N,M = map(int, input().split())
-si,sj,d = map(int, input().split())
-dir = {0:(-1,0), 1:(0,1), 2:(1,0), 3:(0,-1)} # 북 동 남 서 반시계 회전은 (dir-1)%4
+N, M = map(int, input().split())
+dir = {0:(-1,0),1:(0,1),2:(1,0),3:(0,-1)}
+si,sj,sd = map(int, input().split()) # 초기 위치, 방향
+arr = [list(map(int, input().split())) for _ in range(N)]
+# 0인 곳은 청소 x 1은 벽
 
-arr = [list(map(int, input().split())) for _ in range(N)] # 0이 청소안된 빈칸, 1은 벽
-v  = [[0]*M for _ in range(N)]# 청소여부를 체크할 visited 배열
-cnt =0
-while True:
-    if v[si][sj]==0 and arr[si][sj]==0: #로봇 청소기 위치가 빈칸이고, 미청소
-        v[si][sj]=1 # 청소
-        cnt+=1 #청소한 칸 숫자 세기
-
-    flag= True
-    for di,dj in ((1,0),(0,1),(-1,0),(0,-1)): # 4방향 탐색
+cnt = 0
+v = [[0]*M for _ in range(N)]
+def check(si,sj):
+    for i in range(4):
+        di,dj = dir[i]
         ni,nj = si+di, sj+dj
-        if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]==0: #청소안한 빈칸 찾기
-            flag = False
+        if 0<=ni<N and 0<=nj<M and arr[ni][nj]==0 and v[ni][nj]==0:
+            return True
+    return False
 
-    if flag: # 4방향 모두 청소되지 않은 빈칸이 없음.
-        ni,nj = si-dir[d][0], sj-dir[d][1]
-        if 0<=ni<N and 0<=nj<M and arr[ni][nj]!=1:
-            si,sj = ni,nj #후진하고 while문 처음으로 돌아가기
-            continue
-        else: # 후진 불가능하면 반복 종료
-            break
 
-    else: # 청소가능한 칸이 있는 경우
+while True:
+    if v[si][sj]==0 and arr[si][sj]==0: #청소 안한 빈칸
+        cnt+=1
+        v[si][sj]=1
+
+    if check(si,sj):
         for _ in range(4):
-            d = (d-1)%4 # 90도 회전
-            ni,nj = si+dir[d][0], sj+dir[d][1]
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==0 and v[ni][nj]==0:
+            sd = (sd-1)%4
+            ni,nj = si+dir[sd][0], sj+dir[sd][1]
+            if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]==0:
                 si,sj = ni,nj
                 break
-
+    else:
+        ni,nj = si-dir[sd][0], sj-dir[sd][1]
+        if 0<=ni<N and 0<=nj<M and arr[ni][nj]!=1:
+            si,sj = ni,nj
+        else:
+            break
 print(cnt)
