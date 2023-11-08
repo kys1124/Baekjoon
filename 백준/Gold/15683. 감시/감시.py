226c26440a571b5e877a6c8776e6bbc3aadf4575
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-08 20:30:01 +0900

    [Gold IV] Title: 감시, Time: 228 ms, Memory: 112532 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,140 +1,68 @@
 N,M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
+arr = [[6]*(M+2)]+[[6]+list(map(int, input().split()))+[6] for _ in range(N)]+[[6]*(M+2)]
 
 cctv = []
-empty= 0 # 빈칸 개수
-for i in range(N):
-    for j in range(M):
+dir = {1:[(1,0),(0,1),(-1,0),(0,-1)],
+       2:[[(1,0),(-1,0)],[(0,1),(0,-1)]],
+       3:[[(-1,0),(0,1)],[(0,1),(1,0)],[(1,0),(0,-1)],[(0,-1),(-1,0)]],
+       4:[[(0,-1),(-1,0),(0,1)],[(-1,0),(0,1),(1,0)],[(0,1),(1,0),(0,-1)],[(1,0),(0,-1),(-1,0)]],
+       5:[[(0,1),(1,0),(0,-1),(-1,0)]]
+       }
+empty= 0
+v = [[0]*(M+2) for _ in range(N+2)]
+for i in range(1,N+1):
+    for j in range(1,M+1):
         if arr[i][j]!=0 and arr[i][j]!=6:
-            cctv.append((i,j,arr[i][j])) #cctv 리스트에 좌표와 type 저장.
+            cctv.append((i,j,arr[i][j]))
+            v[i][j]=1
+
         elif arr[i][j]==0:
             empty+=1
-dir4 = {0:(1,0),1:(0,1),2:(-1,0),3:(0,-1)}
-dir = {0:[(1,0),(-1,0)],1:[(0,-1),(0,1)]}
-dir1 = {0:[(-1,0),(0,1)], 1:[(0,1),(1,0)], 2:[(1,0),(0,-1)],3:[(0,-1),(-1,0)]}
-dir2 = {0:[(-1,0),(0,1),(1,0)], 1:[(0,1),(1,0),(0,-1)], 2:[(1,0),(0,-1),(-1,0)],3:[(0,-1),(-1,0),(0,1)]}
-ans = 0
-def type1(i,j,t):
-    mul = 1
-    cnt = 0
-    lst = []
-    while True:
-        ni, nj = i + mul * dir4[t][0], j + mul * dir4[t][1]
-        if 0 > ni or 0 > nj or N <= ni or M <= nj or arr[ni][nj] == 6:
-            break
-        elif arr[ni][nj] == 0:
-            arr[ni][nj] = '#'
-            lst.append((ni, nj))
-            cnt += 1
-        mul += 1
-    return cnt, lst
 
-def type2(i,j,t):
-    lst = []
-    cnt = 0
-    for l in range(2):
-        mul=1
-        while True:
-            ni, nj = i + mul * dir[t][l][0], j + mul * dir[t][l][1]
-            if 0 > ni or 0 > nj or N <= ni or M <= nj or arr[ni][nj] == 6:
-                break
-            elif arr[ni][nj] == 0:
-                arr[ni][nj] = '#'
-                lst.append((ni, nj))
-                cnt += 1
-            mul += 1
-    return cnt, lst
-
-def type3(i,j,t):
-    lst = []
-    cnt = 0
-    for l in range(2):
-        mul = 1
-        while True:
-            ni,nj = i+mul*dir1[t][l][0], j+mul*dir1[t][l][1]
-            if 0 > ni or 0 > nj or N <= ni or M <= nj or arr[ni][nj] == 6:
-                break
-            elif arr[ni][nj] == 0:
-                arr[ni][nj] = '#'
-                lst.append((ni, nj))
-                cnt += 1
-            mul += 1
-    return cnt, lst
-
-def type4(i,j,t):
-    lst = []
-    cnt = 0
-    for l in range(3):
-        mul = 1
-        while True:
-            ni,nj = i+mul*dir2[t][l][0], j+mul*dir2[t][l][1]
-            if 0 > ni or 0 > nj or N <= ni or M <= nj or arr[ni][nj] == 6:
-                break
-            elif arr[ni][nj] == 0:
-                arr[ni][nj] = '#'
-                lst.append((ni, nj))
-                cnt += 1
-            mul += 1
-    return cnt, lst
-
-def type5(i,j):
-    lst = []
-    cnt = 0
-    for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
-        mul = 1
-        while True:
-            ni, nj = i + mul * di, j + mul * dj
-            if 0 > ni or 0 > nj or N <= ni or M <= nj or arr[ni][nj] == 6:
-                break
-            elif arr[ni][nj] == 0:
-                arr[ni][nj] = '#'
-                lst.append((ni, nj))
-                cnt += 1
-            mul += 1
-    return cnt, lst
-
-
-
-def dfs(n, sm): #n은 cctv 개수, s는 조합 시작 인덱스, sm은 감시 영역.
-    global ans # 감시가능한 영역 최댓값
+ans = empty
+def dfs(n, v):
+    global ans
     if n==len(cctv):
-        ans = max(ans, sm)
+        sm = 0
+        for i in range(1,N+1):
+            for j in range(1,M+1):
+                if arr[i][j]==0:
+                    sm+=1
+
+        ans = min(ans, sm)
         return
 
-    ci,cj, type = cctv[n][0],cctv[n][1], cctv[n][2]
+    ci,cj, type = cctv[n]
     if type==1:
-        for k in range(4):
-            cnt, lst = type1(ci,cj,k)
-            dfs(n+1, sm+cnt)
-            for ai,aj in lst:
-                arr[ai][aj]=0
-
-    elif type==2:
-        for k in range(2):
-            cnt, lst = type2(ci,cj,k)
-            dfs(n + 1, sm + cnt)
-            for ai, aj in lst:
-                arr[ai][aj] = 0
-
-    elif type==3:
-        for k in range(4):
-            cnt, lst =type3(ci,cj,k)
-            dfs(n + 1, sm + cnt)
-            for ai, aj in lst:
-                arr[ai][aj] = 0
-
-    elif type==4:
-        for k in range(4):
-            cnt, lst = type4(ci,cj,k)
-            dfs(n + 1, sm + cnt)
-            for ai, aj in lst:
-                arr[ai][aj] = 0
-
+        lst = []
+        for di,dj in dir[1]:
+            for mul in range(1,max(N,M)):
+                ni,nj = ci+mul*di, cj+mul*dj
+                if arr[ni][nj]==6:
+                    break
+                elif v[ni][nj]==0:
+                    lst.append((ni,nj))
+                    v[ni][nj]=1
+                    arr[ni][nj]=-1
+            dfs(n+1,v)
+            for i,j in lst:
+                v[i][j]=0
+                arr[i][j]=0
     else:
-        cnt, lst = type5(ci,cj)
-        dfs(n + 1, sm + cnt)
-        for ai, aj in lst:
-            arr[ai][aj] = 0
-
-dfs(0,0)
-print(empty-ans)
+        for i in range(len(dir[type])):
+            lst = []
+            for di,dj in dir[type][i]:
+                for mul in range(max(N, M)):
+                    ni, nj = ci + mul * di, cj + mul * dj
+                    if arr[ni][nj] == 6:
+                        break
+                    elif v[ni][nj] == 0:
+                        lst.append((ni, nj))
+                        v[ni][nj] = 1
+                        arr[ni][nj] = -1
+            dfs(n + 1, v)
+            for i, j in lst:
+                v[i][j] = 0
+                arr[i][j] = 0
+dfs(0,v)
+print(ans)
