692d2360a04cf1ae61adf1f1a6720247544cacba
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-08 20:47:56 +0900

    [Gold V] Title: 치킨 배달, Time: 192 ms, Memory: 112656 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,42 +1,29 @@
-N, M = map(int, input().split())
-
+N , M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
+house =[]
 chicken = []
-house = []
 for i in range(N):
     for j in range(N):
-        if arr[i][j]==2:
-            chicken.append((i,j))
-        elif arr[i][j]==1:
+        if arr[i][j]==1:
             house.append((i,j))
-
-def bfs2(si,sj,lst):
-    mn = 1e9
-    for ci, cj in lst:
-        mn = min(mn, abs(ci-si)+abs(cj-sj))
-    return mn
-
-def dfs(n, sm, lst):
+        elif arr[i][j]==2:
+            chicken.append((i,j))
+ans = 123456789
+def dfs(n,s,lst):
     global ans
-    if sm>=ans:
-        return
-
-    if n==len(house):
-        ans = min(ans, sm)
-        return
-
-    dfs(n+1, sm+bfs2(house[n][0],house[n][1], lst), lst)
-
-def combi(n,s, lst):
     if n==M:
-        dfs(0,0,lst)
+        total_dist = 0
+        for ci,cj in house:
+            dist = N**2
+            for di,dj in lst:
+                cur = abs(ci-di)+abs(cj-dj)
+                if dist>cur:
+                    dist=cur
+            total_dist+=dist
+        ans  = min (ans, total_dist)
         return
 
     for i in range(s,len(chicken)):
-        combi(n+1, i+1, lst+[chicken[i]])
-
-ans = 1e9
-combi(0,0,[])
-
+        dfs(n+1, i+1, lst+[chicken[i]])
+dfs(0,0,[])
 print(ans)
