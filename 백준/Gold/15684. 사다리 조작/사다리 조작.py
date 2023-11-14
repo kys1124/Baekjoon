c1c94e133b41abc04c9592e8f1476b6ae8b5dcb1
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-14 19:37:28 +0900

    [Gold III] Title: 사다리 조작, Time: 540 ms, Memory: 115580 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,81 +1,92 @@
-N, M, H = map(int, input().split()) # 세로선 수, 가로 연결 수, 가로 선 수
+N, M, H  = map(int, input().split())
 arr = [[0]*(2*N+1) for _ in range(2*H+1)]
-col,row = 2*N+1, 2*H+1
-
-for i in range(row):
-    for j in range(1,col,2):
+for i in range(2*H+1):
+    for j in range(1,2*N+1,2):
         arr[i][j]=1
 
 for _ in range(M):
     a,b = map(int, input().split())
     arr[2*a-1][2*b]=1
 
-line = []
-for i in range(1,row-1,2):
-    for j in range(2,col-1,2):
-        if arr[i][j-2]==0 and arr[i][j+2]==0 and arr[i][j]==0:
-            line.append((i,j))
+lst = []
+for i in range(1,2*H+1,2):
+    for j in range(2,2*N,2):
+        if arr[i][j-2]==0 and arr[i][j]==0 and arr[i][j+2]==0:
+            lst.append((i,j))
 
-def check(sj):
-    stk = [(0,sj)]
-    v = [[0]*col for _ in range(row)]
-    v[0][sj]=1
+def dfs(i,arr):
+    stk = [(0,i)]
+    v = [[0]*(2*N+1) for _ in range(2*H+1)]
+    v[0][i]=1
     while stk:
         ci,cj = stk.pop()
         if ci==2*H:
-            if cj==sj:
+            if cj==i:
                 return True
             else:
                 return False
+
         for di,dj in ((0,1),(0,-1),(1,0)):
-            ni,nj = di+ci,dj+cj
-            if 0<=ni<row and 0<=nj<col and v[ni][nj]==0 and arr[ni][nj]==1:
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<2*H+1 and 1<=nj<2*N+1 and arr[ni][nj]==1 and v[ni][nj]==0:
                 stk.append((ni,nj))
                 v[ni][nj]=1
                 break
 
-def check_line(ci,cj):
-    if arr[ci][cj-2]==0 and arr[ci][cj+2]==0:
-        if ci==1 or ci==row-2:
+def check(si,sj, arr):
+    if si==1 or si==2*H-1:
+        if arr[si][sj-2]==0 and arr[si][sj+2]==0:
             return True
-        elif arr[ci-2][cj-2]==1 or arr[ci-2][cj]==1 or arr[ci-2][cj+2]==1\
-            or arr[ci+2][cj-2]==1 or arr[ci+2][cj]==1 or arr[ci+2][cj+2]==1:
-            return True
-    return False
+        else:
+            return False
+    else:
+        if arr[si][sj-2]==0 and arr[si][sj+2]==0:
+            if arr[si-2][sj-2]==1 or arr[si-2][sj]==1 or arr[si-2][sj+2]==1 or arr[si+2][sj-2]==1 or arr[si+2][sj]==1 or arr[si+2][sj+2]==1:
+                return True
+        return False
 
-
-def dfs(n, s, lst,t):
-    global flag, ans
+def dfs2(n,k,s, arr):
+    global flag
     if flag:
-        ans = t
         return
-
-    if n==t:
-        for num in range(1,col,2):
-            if not check(num):
+    if n==k:
+        for i in range(1,2*N,2):
+            if dfs(i,arr):
+                continue
+            else:
                 break
         else:
             flag=True
         return
 
-    for i in range(s, len(line)):
-        ci,cj =  line[i]
-        if check_line(ci,cj):
-            arr[ci][cj]=1
-            dfs(n+1, i+1, lst+[line[i]],t)
-            arr[ci][cj]=0
-t = 0
+    for i in range(s, len(lst)):
+        si,sj = lst[i]
+        if check(si,sj,arr):
+            arr[si][sj]=1
+            dfs2(n+1, k, i+1, arr)
+            arr[si][sj]=0
+
 ans = 4
-while t<=3:
-    flag = False
-    dfs(0,0,[],t)
-    if flag:
-        ans = t
-        break
+# for x in arr:
+#     print(*x)
+
+for i in range(1,2*N,2):
+    if dfs(i, arr):
+        continue
     else:
-        t+=1
+        break
+else:
+    ans = 0
+    print(ans)
 
 if ans==4:
-    print(-1)
-else:
-    print(ans)
+    flag = False
+    for k in range(1,4):
+        dfs2(0,k,0,arr)
+        if flag:
+            ans = k
+            break
+    if ans==4:
+        print(-1)
+    else:
+        print(ans)
