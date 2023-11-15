6d4975692efb2a0645a8c0df22fe6ef1a4dc2eb9
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-15 20:43:55 +0900

    [Gold II] Title: 빵집, Time: 1040 ms, Memory: 349676 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,26 +1,23 @@
 R,C = map(int, input().split())
-arr =[input() for _ in range(R)]
-dir = {0:(-1,1), 1:(0,1),2:(1,1)}
-
-def dfs(ni,nj):
-    global ans, flag
-    if flag:
-        return
-
-    if nj==C-1:
-        ans +=1
-        flag=True
-        return
-
-    if 0<=ni<R and 0<=nj<C and v[ni][nj]==0 and arr[ni][nj]!='x':
-        v[ni][nj]=1
-        for i in range(3):
-            dfs(ni+dir[i][0],nj+1)
-
-
+arr = [list(input()) for _ in range(R)]
 v = [[0]*C for _ in range(R)]
+def dfs(si,sj):
+    stk = [(si,sj)]
+    while stk:
+        ci,cj = stk.pop()
+        if v[ci][cj]==1:
+            continue
+        if cj==C-1:
+            return 1
+        v[ci][cj]=1
+
+        for di,dj in ((1,1),(0,1),(-1,1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<R and 0<=nj<C and v[ni][nj]==0 and arr[ni][nj]=='.':
+                stk.append((ni,nj))
+    return 0
+
 ans = 0
 for i in range(R):
-    flag= False
-    dfs(i,0)
+    ans += dfs(i,0)
 print(ans)
