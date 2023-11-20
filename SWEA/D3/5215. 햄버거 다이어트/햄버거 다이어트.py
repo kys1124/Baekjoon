e1239f1cff22c9adaa196e0fd7b4f2a5bf2f320f
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-20 20:02:23 +0900

    [D3] Title: 햄버거 다이어트, Time: 200 ms, Memory: 63,064 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,18 +1,17 @@
 T = int(input())
 for tc in range(1,1+T):
     N, L = map(int, input().split())
-    cal = [0]
     score = [0]
+    cal = [0]
     for _ in range(N):
-        ti,ki = map(int, input().split())
-        cal.append(ki)
-        score.append(ti)
-
+        t,k = map(int, input().split())
+        score.append(t)
+        cal.append(k)
     dp = [[0]*(L+1) for _ in range(N+1)]
     for i in range(1,N+1):
         for j in range(1,L+1):
             if cal[i]>j:
-                dp[i][j]= dp[i-1][j]
+                dp[i][j] = dp[i-1][j]
             else:
-                dp[i][j] = max(score[i]+dp[i-1][j-cal[i]], dp[i-1][j])
-    print(f'#{tc} {max(map(max,dp))}')
+                dp[i][j] = max(dp[i-1][j-cal[i]]+score[i],dp[i-1][j])
+    print(f'#{tc} {dp[N][L]}')
