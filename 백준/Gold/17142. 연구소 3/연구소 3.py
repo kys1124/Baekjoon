7503b2dd6b6c565151072b4235d4767cd02c3659
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-21 20:23:10 +0900

    [Gold III] Title: 연구소 3, Time: 196 ms, Memory: 111308 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,69 +1,53 @@
-N,M= map(int, input().split())
-arr = [list(map(int , input().split())) for _ in range(N)]
+N, M = map(int, input().split())
+arr =[list(map(int, input().split())) for _ in range(N)]
 
 virus = []
-sm = 0
+empty = 0
 for i in range(N):
     for j in range(N):
-        if arr[i][j]==2:
+        if arr[i][j]==0:
+            empty+=1
+        elif arr[i][j]==2:
             virus.append((i,j))
-        elif arr[i][j]==0:
-            sm+=1
 
-ans = N**2+1
-
-from collections import deque
-s =[]
-def combination(n, next, lst):
-    global s
+ans = N**2
+def dfs(n,s, lst):
+    global ans
     if n==M:
-        s.append(lst)
+        ans = min(ans ,bfs(lst, empty))
         return
 
-    for i in range(next, len(virus)):
-        combination(n+1, i+1, lst+[(virus[i][0],virus[i][1])])
+    for i in range(s,len(virus)):
+        dfs(n+1, i+1, lst+[i])
 
-combination(0,0, [])
-def bfs(s):
-    global sm, ans
-    q = deque(s)
+def bfs(lst, empty):
     v = [[0]*N for _ in range(N)]
-    cnt = 0
-    for i,j in s:
-        v[i][j]=1
+    q = []
+    for i in lst:
+        ci,cj = virus[i]
+        q.append((ci,cj))
+        v[ci][cj]=1
     t = 0
+    if empty==0:
+        return 0
     while q:
-        if cnt==sm:
-            return t
-
-        if t>=ans:
-            return ans
-
-        for _ in range(len(q)):
-            ci,cj = q.popleft()
-            for di,dj in ((-1,0),(1,0),(0,1),(0,-1)):
-                ni,nj = ci+di,cj+dj
-                if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]==0:
+        temp_q = []
+        for i in range(len(q)):
+            ci,cj = q[i]
+            for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
+                ni,nj = ci+di, cj+dj
+                if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]!=1:
                     v[ni][nj]=1
-                    cnt +=1
-                    q.append((ni,nj))
-                elif 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]==2:
-                    v[ni][nj]=1
-                    q.append((ni,nj))
-        t += 1
-
-    if cnt!=sm:
-        return -1
-
-
-for x in s:
-    time = bfs(x)
-    if time==-1:
-        continue
-    else:
-        ans = time
-
-if ans==N**2+1:
+                    temp_q.append((ni,nj))
+                    if arr[ni][nj]==0:
+                        empty-=1
+        if empty==0:
+            return t+1
+        t+=1
+        q = temp_q
+    return N**2
+dfs(0,0,[])
+if ans==N**2:
     print(-1)
 else:
     print(ans)
