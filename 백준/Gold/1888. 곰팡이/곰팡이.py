e92242f8ef310dc8274a3b95894b285fc4d00219
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-22 17:36:21 +0900

    [Gold III] Title: 곰팡이, Time: 224 ms, Memory: 112608 KB -BaekjoonHub

--- 백준/Gold/1888. 곰팡이/곰팡이.py
+++ 백준/Gold/1888. 곰팡이/곰팡이.py
@@ -1,42 +1,40 @@
-N,M = map(int, input().split())
-arr =[list(map(int, input())) for _ in range(N)]
-
-def dfs(si,sj,idx):
+N,M  = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(N)]
+def dfs(si,sj, idx):
     stk = [(si,sj)]
-    v[si][sj]=idx
+    v[si][sj] = idx
     while stk:
         ci,cj = stk.pop()
-
         for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
-            ni,nj = ci+di,cj+dj
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]!=0 and v[ni][nj]==0:
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]>0:
                 v[ni][nj]=idx
                 stk.append((ni,nj))
 
-ans = 0
+
+t = 0
 while True:
-    idx = 1
-    v =[[0]*M for _ in range(N)]
+    idx= 1
+    v = [[0]*M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            if v[i][j]==0 and arr[i][j]>0:
+            if arr[i][j]>0 and v[i][j]==0:
                 dfs(i,j,idx)
                 idx+=1
 
     if idx==2:
+        print(t)
         break
-    ans +=1
+    t+=1
 
-    new = [[0]*M for _ in range(N)]
-
+    copy_arr = [arr[i][:] for i in range(N)]
     for i in range(N):
         for j in range(M):
-            if v[i][j]>0:
-                k = arr[i][j]
-                for si in range(max(0,i-k), min(N,i+k+1)):
-                    for sj in range(max(0,j-k), min(M,j+k+1)):
-                        if new[si][sj]<k:
-                            new[si][sj]=k
-
-    arr = new
-print(ans)
+            if copy_arr[i][j]>0:
+                k = copy_arr[i][j]
+                si,sj = i-k,j-k
+                ei,ej = i+k, j+k
+                for ci in range(si,ei+1):
+                    for cj in range(sj, ej+1):
+                        if 0<=ci<N and 0<=cj<M and arr[ci][cj]<k:
+                            arr[ci][cj]=k
