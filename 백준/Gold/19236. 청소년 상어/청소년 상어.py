eb19ac84da36db6bc4835dc1718406240ac786c3
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-28 19:24:37 +0900

    [Gold II] Title: 청소년 상어, Time: 156 ms, Memory: 113076 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.py
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.py
@@ -1,73 +1,67 @@
 import copy
-fish_dic = dict()
-arr = [[0]*4 for _ in range(4)]
 dir = {0:(-1,0),1:(-1,-1),2:(0,-1),3:(1,-1),4:(1,0),5:(1,1),6:(0,1),7:(-1,1)}
-
+fish = {}
+arr =[[0]*4 for _ in range(4)]
 for i in range(4):
     lst = list(map(int, input().split()))
-    for j in range(0,8,2):
-        a,b = lst[j], lst[j+1]
-        arr[i][j//2] = a
-        fish_dic[a] = [i,j//2, b-1]
+    for j in range(4):
+        arr[i][j] = lst[2*j]
+        fish[lst[2*j]] = [i,j,lst[2*j+1]-1]
 
-si,sj,sd = 0,0,fish_dic[arr[0][0]][2]
-ans = arr[0][0]
-del fish_dic[arr[0][0]]
-arr[0][0] = 0
+si,sj = 0,0
+ans = arr[si][sj]
+sd = fish[arr[si][sj]][2]
+del fish[arr[si][sj]]
+arr[si][sj] = 0
 
-def move_fish(si,sj):
-    for num in range(1,17):
-        if fish_dic.get(num):
-            ci,cj,cd = fish_dic[num]
+def dfs(si,sj,sd,arr, sm):
+    global ans, fish
+    for i in range(1,17):
+        if fish.get(i): # 살아있는 물고기 이면,
+            ci,cj,cd = fish[i]
+            c_num = i
             for _ in range(8):
-                ni,nj = ci+dir[cd][0],cj+dir[cd][1]
-                if 0<=ni<4 and 0<=nj<4 and (ni,nj)!=(si,sj):
-                    if arr[ni][nj]==0:
-                        arr[ci][cj]=0
-                        arr[ni][nj]=num
-                        fish_dic[num] = [ni,nj,cd]
-                    else:
-                        change_num = arr[ni][nj]
-                        arr[ni][nj],arr[ci][cj] = arr[ci][cj], arr[ni][nj]
-                        fish_dic[change_num][0], fish_dic[change_num][1] = ci,cj
-                        fish_dic[num] = [ni,nj,cd]
+                ni,nj = ci+dir[cd][0], cj+dir[cd][1]
+                if 0<=ni<4 and 0<=nj<4 and (ni,nj)!=(si,sj) and arr[ni][nj]>0:
+                    other = arr[ni][nj]
+                    od = fish[other][2]
+                    fish[c_num] = [ni,nj,cd]
+                    fish[other] = [ci,cj,od]
+                    arr[ni][nj] = c_num
+                    arr[ci][cj] = other
+                    break
+                elif 0<=ni<4 and 0<=nj<4 and (ni,nj)!=(si,sj) and arr[ni][nj]==0:
+                    fish[c_num] = [ni,nj,cd]
+                    arr[ni][nj] = c_num
+                    arr[ci][cj]=  0
                     break
                 else:
                     cd = (cd+1)%8
-                    fish_dic[num][2] = cd
+                    fish[c_num][2] = cd
 
 
-def move_shark(si,sj,sd):
-    lst =[]
+
+    lst = []
     for mul in range(1,4):
         ni,nj = si+mul*dir[sd][0], sj+mul*dir[sd][1]
-        if 0<=ni<4 and 0<=nj<4 and arr[ni][nj]!=0:
+        if 0<=ni<4 and 0<=nj<4 and arr[ni][nj]>0:
             lst.append((ni,nj))
-    return lst
-
-
-
-def dfs(n,si,sj,sd,cnt):
-    global ans, fish_dic,arr
-
-    move_fish(si,sj)
-
-    lst = move_shark(si, sj, sd)
 
     if not lst:
-        ans = max(ans, cnt)
+        ans = max(ans, sm)
         return
+    else:
+        for ni,nj in lst:
+            copy_arr = [arr[i][:] for i in range(4)]
+            copy_fish = copy.deepcopy(fish)
+            num = arr[ni][nj]
+            nd = fish[num][2]
+            del fish[num]
+            arr[ni][nj] = 0
+            dfs(ni,nj,nd,arr,sm+num)
+            fish = copy_fish
+            arr = copy_arr
 
-    for ni,nj in lst:
-        copy_fish_dic = copy.deepcopy(fish_dic)
-        copy_arr = [arr[i][:] for i in range(4)]
-        fish_num = copy_arr[ni][nj]
-        nd = fish_dic[fish_num][2]
-        arr[ni][nj]=0
-        del fish_dic[fish_num]
-        dfs(n+1,ni,nj,nd, cnt+fish_num)
-        arr = copy_arr
-        fish_dic = copy_fish_dic
 
-dfs(0,0,0,sd,ans)
+dfs(si,sj,sd,arr,ans)
 print(ans)
