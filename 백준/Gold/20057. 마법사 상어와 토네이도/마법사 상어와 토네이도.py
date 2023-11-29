ec66f3b9161ffe2d6e2fb8ff73af26ec730c7a36
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-29 18:21:55 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 264 ms, Memory: 115292 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,54 +1,43 @@
-N = int(input()) # 배열 가로 세로 길이, 홀수
-arr = [list(map(int, input().split())) for _ in range(N)] # 모래양이 저장된 배열
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-v = [[0]*N for _ in range(N)] #토네이도 이동 경로 방문 배열
+dir = {0:(0,-1),1:(1,0),2:(0,1),3:(-1,0)}
+percentage = [1,1,2,7,7,2,10,10,5,'a']
 
-dir = {0:(0,-1),1:(1,0),2:(0,1),3:(-1,0)} #토네이도 이동 방향 좌 하 우 상
+dic = {0:[(-1,0),(1,0),(-2,-1),(-1,-1),(1,-1),(2,-1),(-1,-2),(1,-2),(0,-3),(0,-2)]}
+for i in range(3):
+    lst = []
+    for di,dj in dic[i]:
+        lst.append((-dj,di))
+    dic[i+1] = lst
 
-sand0 = {0:(-1,0),1:(1,0),2:(-1,-1),3:(1,-1),4:(-2,-1),5:(+2,-1),6:(-1,-2),7:(+1,-2),8:(0,-3), 9:(0,-2)} #d==0 기준.
-def turn(d, sand):
-    if d == 0:
-        return sand
-    elif d == 1:
-        new_sand = {}
-        for key, value in sand.items():
-            new_sand[key] = (-value[1], value[0])
-        return new_sand
-sand1 = turn(1, sand0)
-sand2 = turn(1, sand1)
-sand3 = turn(1, sand2)
-sand = [sand0, sand1, sand2, sand3]
-percentage = [1,1,7,7,2,2,10,10,5]
-
-si,sj = N//2, N//2 # 격자의 중심이 토네이도 시작 위치.
-d = 3 # 초기 토네이도 방향은 왼쪽
-sm = 0
-
-for i in range(1,N**2+1):
-    v[si][sj]=i
-    if v[si+dir[(d+1)%4][0]][sj+dir[(d+1)%4][1]]!=0:
-        ni,nj = si+dir[d][0], sj+dir[d][1]
+si,sj =N//2, N//2
+sd = 0
+v = [[0]*N for _ in range(N)]
+v[si][sj]=1
+ans = 0
+for num in range(1,N**2):
+    oi,oj = si+dir[sd][0], sj+dir[sd][1]
+    v[oi][oj]=num
+    cur_amt = arr[oi][oj]
+    total = 0
+    for i in range(9):
+        di,dj = dic[sd][i]
+        ni,nj = si+di,sj+dj
+        lose = cur_amt * percentage[i] // 100
+        total += lose
+        if 0<=ni<N and 0<=nj<N:
+            arr[ni][nj]+=lose
+        else:
+            ans+=lose
+    di,dj = dic[sd][9]
+    ni,nj = si+di, sj+dj
+    lose = cur_amt - total
+    if 0<=ni<N and 0<=nj<N:
+        arr[ni][nj]+= lose
     else:
-        d = (d+1)%4
-        ni,nj = si+dir[d][0], sj+dir[d][1]
-
-    # si,sj,d # 토네이도 좌표와 이동 방향.
-    #모래가 있는 칸: ni,nj
-    cur_sand = arr[ni][nj]
-    for idx in range(10):
-        nni,nnj = si+sand[d][idx][0],sj+sand[d][idx][1]
-        if idx==9:
-            moving_sand = arr[ni][nj]
-        else:
-            moving_sand = (cur_sand * percentage[idx]) // 100
-
-        arr[ni][nj] -= moving_sand
-
-        if 0<=nni<N and 0<=nnj<N:
-            arr[nni][nnj]+= moving_sand
-        else:
-            sm+= moving_sand
-
-    si,sj = ni,nj
-
-print(sm)
+        ans+=lose
+    si,sj = oi,oj
+    if v[si+dir[(sd+1)%4][0]][sj+dir[(sd+1)%4][1]]==0:
+        sd = (sd+1)%4
+print(ans)
