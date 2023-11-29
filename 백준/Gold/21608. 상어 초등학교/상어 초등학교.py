c39542f27c03f800b1e6672e161d7a266f6c36b4
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-29 19:36:35 +0900

    [Gold V] Title: 상어 초등학교, Time: 196 ms, Memory: 112480 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,57 +1,46 @@
 N = int(input())
+dic = {}
 arr =[[0]*N for _ in range(N)]
-dic = dict()
 for _ in range(N**2):
-    c,n1,n2,n3,n4 = map(int, input().split())
-    dic[c] = set([n1,n2,n3,n4])
-    si,sj,mxlike,mxempty = N,N,-1,-1
+    student, *like_lst = list(map(int, input().split()))
+    dic[student] = set(like_lst)
+
+    mi,mj,mx_empty, mx_like = N,N,-1,-1
     for i in range(N):
         for j in range(N):
             if arr[i][j]==0:
-                like =0
-                empty=0
-                for di,dj in ((-1,0),(1,0),(0,1),(0,-1)):
+                empty, like = 0,0
+                for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
                     ni,nj = i+di, j+dj
                     if 0<=ni<N and 0<=nj<N:
-                        if arr[ni][nj] in dic[c]:
-                            like+=1
                         if arr[ni][nj]==0:
                             empty+=1
+                        elif arr[ni][nj] in dic[student]:
+                            like+=1
+                if like>mx_like:
+                    mx_empty, mx_like, mi, mj = empty, like, i, j
+                elif like==mx_like:
+                    if empty>mx_empty:
+                        mx_empty, mi,mj = empty, i, j
+                    elif empty==mx_empty:
+                        if i<mi:
+                            mi,mj = i,j
+                        elif i==mi:
+                            if j<mj:
+                                mj = j
 
-                if like>mxlike:
-                    mxlike= like
-                    mxempty= empty
-                    si,sj = i,j
-
-                elif like==mxlike:
-                    if empty>mxempty:
-                        mxempty=empty
-                        si,sj=i,j
-                    elif mxempty==empty:
-                        if si>i:
-                            si,sj =i,j
-                        elif si==i:
-                            if sj>j:
-                                si,sj =i,j
-
-    arr[si][sj]=c
+    arr[mi][mj] = student
 
 ans = 0
 for i in range(N):
     for j in range(N):
-        temp = 0
-        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-            ni, nj = i + di, j + dj
-            if 0 <= ni < N and 0 <= nj < N:
-                if arr[ni][nj] in dic[arr[i][j]]:
-                    temp +=1
-
-        if temp==1:
-            ans +=temp
-        elif temp==2:
-            ans +=10
-        elif temp==3:
-            ans +=100
-        elif temp==4:
-            ans +=1000
+        like=0
+        for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
+            ni,nj = di+i, dj+j
+            if 0<=ni<N and 0<=nj<N and arr[ni][nj] in dic[arr[i][j]]:
+                like+=1
+        if like==0:
+            continue
+        else:
+            ans+= 10**(like-1)
 print(ans)
