5236dc96564f845cd71fb88b1cf1a6ed87ecf5b5
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-30 21:02:20 +0900

    [Gold I] Title: 마법사 상어와 블리자드, Time: 208 ms, Memory: 112396 KB -BaekjoonHub

--- 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.py
+++ 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.py
@@ -1,106 +1,92 @@
-N, M = map(int, input().split()) # M개 마법 실행.
-# 상어 위치는 N//2, N//2 정 중앙에 있음.
-# 상어는 토네이도로 움직여야함 벽 때문에.
-arr = [list(map(int, input().split())) for _ in range(N)] # N x N 격자
+N, M = map(int, input().split()) #N*N 격자, M번 반복
+arr = [list(map(int, input().split())) for _ in range(N)]
+v = [[0]*N for _ in range(N)]
 dir = {0:(0,-1),1:(1,0),2:(0,1),3:(-1,0)}
-magic_d = {1:(-1,0),2:(1,0),3:(0,-1),4:(0,1)}
-score = [0,0,0]
-magic = []
+magic_dir = {1:(-1,0),2:(1,0),3:(0,-1),4:(0,1)}
+si,sj,sd = N//2, N//2,0
+v[si][sj]=1
+tornado = []
+ans = 0
 
-def check(q):
-    if not q:
-        return False
+def check(ball): #4개 이상 연속하는 구슬 체크
+    global ans
+    lst =[]
+    start= 0
+    end = 0
+    color = ball[0]
     cnt = 1
-    pre = q[0]
-    for i in range(1,len(q)):
-        if cnt>=4:
-            return True
-
-        if pre==q[i]:
+    for i in range(1,len(ball)):
+        if ball[i]==color:
             cnt+=1
-        else:
+            end+=1
+        else: #다른 색.
+            if cnt>=4: #이 구슬 이전까지 같은 색
+                lst.extend([num for num in range(start, end+1,1)])
+                ans+=color*cnt
+            start=end=i
+            color=ball[i]
             cnt=1
-            pre=q[i]
-    return False
+    if cnt>=4:
+        lst.extend([num for num in range(start, end + 1,1)])
+        ans += color * cnt
+    return lst
 
-def pang(q):
-    stk = []
-    idx = 0
-    while idx<len(q): # 구슬 폭발
-        cnt = 1
-        start = idx
-        while idx<len(q)-1 and q[idx]==q[idx+1]:
-            idx+=1
-            cnt+=1
-        if cnt<4:
-            for i in range(start, start+cnt):
-                stk.append(q[i])
-        else:
-            score[q[idx]-1]+=cnt #폭발한 구슬 인덱스로 하고 개수 리스트에 저장.
-        idx+=1
-    return stk
+for i in range(1,N**2):
+    si,sj = si+dir[sd][0], sj+dir[sd][1]
+    v[si][sj]=i
+    tornado.append((si,sj))
+    nd = (sd+1)%4
+    if v[si+dir[nd][0]][sj+dir[nd][1]]==0:
+        sd = nd
 
+si,sj = N//2, N//2
 for _ in range(M):
     d,s = map(int, input().split())
-    si, sj, sd = N//2, N//2,3 # 상어 위치
-    ci,cj = si,sj
-    for mul in range(1,s+1): #블리자드 시전 구슬 0으로 바꿔치기
-        ni,nj = si+mul*magic_d[d][0], sj+mul*magic_d[d][1]
-        if 0<=ni<N and 0<=nj<N:
+
+    for mul in range(s+1): #블리자드 마법
+        ni,nj = si+mul*magic_dir[d][0], sj+mul*magic_dir[d][1]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]>0:
             arr[ni][nj]=0
-        else:
+
+    ball = []
+    for ci,cj in tornado: #구슬 앞으로 당기기
+        if arr[ci][cj]>0:
+            ball.append(arr[ci][cj])
+
+    while ball:
+        pang_lst = check(ball)
+        if not pang_lst:
             break
+        new_ball = []
+        idx = 0
+        pang_lst = set(pang_lst)
+        for i in range(len(ball)):
+            if i not in pang_lst:
+                new_ball.append(ball[i])
+        ball = new_ball
+    if not ball:
+        break
 
-    q = []
-    v =[[0]*N for _ in range(N)]
-    for i in range(1,N**2+1): # 토네이도 길을 따라 가면서 구슬 모으기
-        v[ci][cj]=i
-        if v[ci+dir[(sd+1)%4][0]][cj+dir[(sd+1)%4][1]]==0:
-            sd = (sd+1)%4
-        ci,cj = ci+dir[sd][0], cj+dir[sd][1]
-        if arr[ci][cj]>0: #구슬이 있다면 q에 담기
-            q.append((arr[ci][cj]))
-
-    while check(q):
-        stk = pang(q)
-        q= stk
-
-    # 구슬 변화
-
-    new_q = []
-    if q:
-        cnt = 1
-        pre = q[0]
-        for i in range(1,len(q)):
-            if q[i]==pre:
-                cnt+=1
-            else:
-                if len(new_q)<=N**2-3:
-                    new_q.append(cnt)
-                    new_q.append(pre)
-                elif len(new_q)==N**2-2:
-                    new_q.append(cnt)
-                    break
-                cnt=1
-                pre=q[i]
+    color = ball[0]
+    cnt = 1
+    new_ball  = []
+    for i in range(1,len(ball)):
+        if color==ball[i]:
+            cnt+=1
         else:
-            new_q.append(cnt)
-            new_q.append(pre)
-
+            new_ball.append(cnt)
+            new_ball.append(color)
+            color = ball[i]
+            cnt=1
+    new_ball.append(cnt)
+    new_ball.append(color)
 
     new_arr = [[0]*N for _ in range(N)]
-    si,sj,sd = N//2, N//2,3
-    new_arr[si][sj]=1
-    for i in range(1,len(new_q)+1): # 토네이도 길을 따라 가면서 구슬 모으기
-        if new_arr[si+dir[(sd+1)%4][0]][sj+dir[(sd+1)%4][1]]==0:
-            sd = (sd+1)%4
-        si,sj = si+dir[sd][0], sj+dir[sd][1]
-        if not (0<=si<N and 0<=sj<N) or new_arr[si][sj]!=0:
+    for i in range(len(tornado)):
+        if i<len(new_ball):
+            new_arr[tornado[i][0]][tornado[i][1]] = new_ball[i]
+        else:
             break
-        new_arr[si][sj]=new_q[i-1]
+    arr = new_arr
 
-    new_arr[N//2][N//2]=0
-    arr= new_arr
-
-print(score[0]+score[1]*2+score[2]*3)
-
+print(ans)
