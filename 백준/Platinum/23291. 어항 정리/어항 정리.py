92c93dd07a02f25e4b8ee295063a6c4225c3d6ed
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-12-04 21:25:51 +0900

    [Platinum V] Title: 어항 정리, Time: 136 ms, Memory: 110784 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.py
+++ 백준/Platinum/23291. 어항 정리/어항 정리.py
@@ -1,85 +1,95 @@
-N, K = map(int, input().split()) # N개 어항, K는 가장 많은 어항과 적은 어항의 차이가 K 이하
-arr= list(map(int, input().split())) #어항의 물고기 초기 수
+
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
 
 
-def addfish(arr): # 1XN짜리 arr #가장 적은 물고기가 있는 어항 찾아서 1마리씩 더하기
-    mn = min(arr)
-    for i in range(len(arr)):
-        if arr[i]==mn:
-            arr[i]+=1
+def smallest(lst):
+    mn = min(lst)
+    for i in range(N):
+        if lst[i]==mn:
+            lst[i]+=1
+    return lst
 
-def put(arr):
-    on = [[arr[0]]]
-    remain = arr[1:]
-    while len(remain)-len(on[0])>=len(on)+1:
-        lst = []
+def make(lst):
+    on = [[lst[0]]]
+    remain = lst[1:]
+    while len(on)+1<=len(remain)-len(on[0]):
+        new = []
         for _ in range(len(on[0])):
-            lst.append(remain.pop(0))
-        on.append(lst)
+            new.append(remain.pop(0))
+        on.append(new)
+        on.reverse()
         on = list(map(list, zip(*on)))
-        for x in on:
-            x.reverse()
-
     if remain:
         on.append(remain)
     return on
 
+mx,mn = max(lst), min(lst)
+t = 0
+while mx-mn>K:
+    lst = smallest(lst)
+    lst = make(lst)
 
-def adjust(arr):
-    copy_arr = [arr[i][:] for i in range(len(arr))]
-
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
+    copy_arr = [lst[i][:] for i in range(len(lst))]
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
             for di,dj in ((1,0),(0,1)):
                 ni,nj = i+di, j+dj
-                if 0<=ni<len(arr) and 0<=nj<len(arr[i]):
-                    d = abs(copy_arr[i][j]-copy_arr[ni][nj])//5
-                    if d>0:
+                if 0<=ni<len(lst) and 0<=nj<len(lst[i]):
+                    diff = abs(copy_arr[i][j]-copy_arr[ni][nj])//5
+                    if diff>0:
                         if copy_arr[i][j]>copy_arr[ni][nj]:
-                            arr[i][j]-=d
-                            arr[ni][nj]+=d
+                            lst[i][j]-=diff
+                            lst[ni][nj]+=diff
                         elif copy_arr[i][j]<copy_arr[ni][nj]:
-                            arr[i][j]+=d
-                            arr[ni][nj]-=d
-    return arr
+                            lst[i][j] += diff
+                            lst[ni][nj] -= diff
 
-def make_line(arr):
-    lst = []
-    for j in range(len(arr[0])):
-        for i in range(len(arr)-1,-1,-1):
-            lst.append(arr[i][j])
-    lst += arr[-1][len(arr[0]):]
-    return lst
-
-def put2(arr):
     new = []
-    lst  = arr[:N//2]
-    lst.reverse()
-    new.append(lst)
-    new.append(arr[N//2:])
-    new2 =[]
+    for j in range(len(lst[0])):
+        for i in range(len(lst)-1,-1,-1):
+            new.append(lst[i][j])
+    for j in range(len(lst[0]),len(lst[-1])):
+        new.append(lst[-1][j])
+
+    lst = new
+
+    new = []
+    left, right = lst[:N//2], lst[N//2:]
+    left.reverse()
+    new.append(left)
+    new.append(right)
+
+    arr1 = []
+    arr2 = []
     for i in range(1,-1,-1):
-        lst = new[i][:N//4]
-        lst.reverse()
-        new2.append(lst)
-    for i in range(2):
-        new2.append(new[i][N//4:])
+        left,right = new[i][:N//4], new[i][N//4:]
+        left.reverse()
+        arr1.append(left)
+        arr2.insert(0,right)
 
-    return new2
+    lst = arr1+arr2
+    copy_arr = [lst[i][:] for i in range(len(lst))]
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            for di, dj in ((1, 0), (0, 1)):
+                ni, nj = i + di, j + dj
+                if 0 <= ni < len(lst) and 0 <= nj < len(lst[i]):
+                    diff = abs(copy_arr[i][j] - copy_arr[ni][nj]) // 5
+                    if diff > 0:
+                        if copy_arr[i][j] > copy_arr[ni][nj]:
+                            lst[i][j] -= diff
+                            lst[ni][nj] += diff
+                        elif copy_arr[i][j] < copy_arr[ni][nj]:
+                            lst[i][j] += diff
+                            lst[ni][nj] -= diff
 
-T = 0
-while True:
-    mxfish, mnfish = max(arr), min(arr)
-    if mxfish - mnfish <= K:
-        break
-    addfish(arr)
-    arr = put(arr)
-    arr = adjust(arr)
-    arr = make_line(arr)
-    arr = put2(arr)
-    arr = adjust(arr)
-    arr = make_line(arr)
+    new = []
+    for j in range(len(lst[0])):
+        for i in range(len(lst)-1,-1,-1):
+            new.append(lst[i][j])
+    lst = new
+    mx,mn = max(lst), min(lst)
+    t+=1
 
-    T +=1
-
-print(T)
+print(t)
