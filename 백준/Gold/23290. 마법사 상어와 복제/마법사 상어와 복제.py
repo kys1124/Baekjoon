e069eadccd3bf9b2cd42038be8ff36d7c34fbc36
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-12-04 19:07:12 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 500 ms, Memory: 174884 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
@@ -1,88 +1,85 @@
-# 4x4 격자에서 M마리 물고기 각 칸에 있고, 이동방향
-dir = {0:(0,-1),1:(-1,-1),2:(-1,0),3:(-1,1),4:(0,1),5:(1,1),6:(1,0),7:(1,-1)} #물고기 이동 방향
-shark_dir = {1:(-1,0),2:(0,-1),3:(1,0),4:(0,1)}
-
-M,S = map(int, input().split()) #M마리 S번 반복
-fish = dict()
-v = [[0]*4 for _ in range(4)]
+import copy
+M,S = map(int, input().split()) #M마리, 마법횟수 S
+fish= dict()
+dir = {0:(0,-1),1:(-1,-1),2:(-1,0),3:(-1,1),4:(0,1),5:(1,1),6:(1,0),7:(1,-1)}
 for _ in range(M):
-    fx,fy,d = map(lambda x:int(x)-1, input().split()) #0,0 3,3 까지 0~7까지 방향으로 바꾸기.
+    fx,fy,d = map(lambda x:int(x)-1, input().split())
     if fish.get((fx,fy)):
         fish[(fx,fy)].append(d)
     else:
-        fish[(fx,fy)] =[d]
+        fish[(fx,fy)] = [d]
 
-si,sj = map(lambda x:int(x)-1, input().split()) # 상어 위치.
+v =[[0]*4 for _ in range(4)]
 
-def dfs(n, si,sj, cnt, d):
-    global mxcnt, new_i,new_j, mxd
+si,sj = map(lambda x:int(x)-1, input().split())
+shark = {1:(-1,0),2:(0,-1),3:(1,0),4:(0,1)}
+
+
+def dfs(n,si,sj, sm, move_lst, fish):
+    global shark_move, eat
     if n==3:
-        if mxcnt<cnt:
-            new_i,new_j,mxd, mxcnt = si,sj, int(d), cnt
-
-        elif mxcnt==cnt:
-            if mxd>int(d):
-                new_i,new_j,mxd = si,sj, int(d)
+        if sm>eat:
+            eat = sm
+            shark_move = move_lst
         return
 
     for i in range(1,5):
-        ni,nj = si+shark_dir[i][0], sj+shark_dir[i][1]
+        di,dj = shark[i]
+        ni,nj = si+di,sj+dj
         if 0<=ni<4 and 0<=nj<4:
+            num = 0
             if fish.get((ni,nj)):
-                lst = fish[(ni,nj)][:]
+                co = fish[(ni,nj)][:]
+                num += len(co)
                 del fish[(ni,nj)]
-                dfs(n+1, ni,nj, cnt+len(lst), d+str(i))
-                fish[(ni,nj)] = lst
+                dfs(n+1, ni,nj, sm+num, move_lst+[(ni,nj)], fish)
+                fish[(ni,nj)] = co
             else:
-                dfs(n+1, ni,nj, cnt,  d+str(i))
+                dfs(n+1, ni,nj, sm, move_lst+[(ni,nj)], fish)
 
 for _ in range(S):
-    copy_fish = fish #물고기 복제
-
-    new_fish=  dict()
-    for key, value in fish.items():
+    copy_fish = fish
+    new_fish= dict()
+    for key,value in fish.items():
         ci,cj = key
-        fish_lst = value
-        for cd in fish_lst:
+        for cd in value:
             for _ in range(8):
-                ni,nj = ci+dir[cd][0], cj+dir[cd][1]
+                ni,nj = ci+dir[cd][0],cj+dir[cd][1]
                 if 0<=ni<4 and 0<=nj<4 and v[ni][nj]==0 and (ni,nj)!=(si,sj):
                     if new_fish.get((ni,nj)):
                         new_fish[(ni,nj)].append(cd)
                     else:
-                        new_fish[(ni,nj)]=[cd]
+                        new_fish[(ni,nj)] = [cd]
                     break
                 else:
                     cd = (cd-1)%8
             else:
-                if new_fish.get((ci,cj)):
+                if new_fish.get((ci, cj)):
                     new_fish[(ci,cj)].append(cd)
                 else:
-                    new_fish[(ci,cj)]=[cd]
+                    new_fish[(ci, cj)] = [cd]
 
-    fish = new_fish
-    new_i,new_j, mxcnt, mxd = -1,-1,0,555
-    dfs(0,si,sj,0,'')
-
+    shark_move, eat = [], -1
+    dfs(0,si,sj, 0, [], new_fish)
     for i in range(4):
         for j in range(4):
             if v[i][j]>0:
                 v[i][j]-=1
 
-    for i in str(mxd):
-        si,sj = si+shark_dir[int(i)][0], sj+shark_dir[int(i)][1]
-        if fish.get((si,sj)):
-            v[si][sj]=2
-            del fish[(si,sj)]
+    for ci,cj in shark_move:
+        if new_fish.get((ci,cj)):
+            del new_fish[(ci,cj)]
+            v[ci][cj] = 2
+        si,sj = ci,cj
 
+    fish = new_fish
     for key, value in copy_fish.items():
-        ci,cj = key
-        if fish.get((ci,cj)):
-            fish[(ci,cj)]+=value
+        if fish.get(key):
+            fish[key]+=value
         else:
-            fish[(ci,cj)]=value
+            fish[key] = value
 
-sm = 0
+ans = 0
 for value in fish.values():
-    sm +=len(value)
-print(sm)
+    ans+=len(value)
+print(ans)
