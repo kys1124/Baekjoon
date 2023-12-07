7b253b907c91834f222d150d37079611b9ae12a7
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-12-07 13:21:32 +0900

    [Platinum V] Title: 조별과제 멈춰!, Time: 1392 ms, Memory: 140720 KB -BaekjoonHub

--- 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.py
+++ 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.py
@@ -1,5 +1,5 @@
-import sys
 import heapq
+from collections import deque
 N, M = map(int,input().split())
 adj = [[] for _ in range(N+1)]
 for _ in range(M):
@@ -11,44 +11,40 @@ def prim():
     v = set()
     mst = [[] for _ in range(N+1)]
     pq = [(0,1,0)]
+    total  = 0
     while pq:
         dist, cur, pre = heapq.heappop(pq)
 
         if cur in v:
             continue
         v.add(cur)
+        total+= dist
         if pre!=0:
             mst[cur].append((pre,dist))
             mst[pre].append((cur,dist))
 
         if len(v)==N:
-            return mst
+            return mst, total
 
         for nxt, nxt_dist in adj[cur]:
             if nxt not in v:
                 heapq.heappush(pq, (nxt_dist,nxt,cur))
 
-mst = prim()
-def bfs(x,y):
-    q = [(0,x),(0,y)]
-    v = [0]*(N+1)
-    heapq.heapify(q)
-    cnt= 0
-    total = 0
-    while q:
-        cost, cur = heapq.heappop(q)
-        if v[cur]==1:
-            continue
-        v[cur]=1
-        cnt+=1
-        total+=cost
+mst,total = prim()
 
-        if cnt==N:
-            return total
-        for nxt,nxt_cost in mst[cur]:
+def bfs2(x,y):
+    q = deque([(x,0)])
+    v = [0]*(N+1)
+    v[x]=1
+    while q:
+        cur,score = q.popleft()
+        if cur==y:
+            return score
+        for nxt, nxt_cost in mst[cur]:
             if v[nxt]==0:
-                heapq.heappush(q,(nxt_cost,nxt))
+                v[nxt]=1
+                q.append((nxt,max(score,nxt_cost)))
 
 for _ in range(int(input())):
     x,y = map(int, input().split())
-    print(bfs(x,y))
+    print(total-bfs2(x,y))
